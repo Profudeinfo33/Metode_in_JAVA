7bae1334a09a62a142f2a40cbed598275584fd36
Dinu Petre <dinu26petre@gmail.com>
2024-03-14 14:53:11 +0200

    Deschidere proiect nou in Intelij

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Suma(10,20);
+    }
+
+    public static void Suma(int numar1,int numar2){
+        System.out.println(numar1+numar2);
     }
 }
